ae018b22aea45f5194b258c9f36974186e7a8196
Dmitri Popov <dmpop@linux.com>
2019-01-07 18:41:56 +0100

    Add table sort

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
 	 }
 	 th {
 	     font-weight: 600;
+         cursor: pointer;
 	 }
 	 td {
 	     text-align: left;
@@ -53,7 +54,6 @@
     <body>
 	<div id="content">
 	    <h1>Everyday Photo Carry</h1>
-	    <p><button onclick="sortTable()" >Sort</button></p>
 	    <table id="theTable" class="pure-table pure-table-horizontal">
 		<?php
 		$CSVFILE = "data.csv";
@@ -75,7 +75,7 @@
 			    $value2 = $data[2];
 			}
 			if ($row == 1) {
-			    echo '<th>' . $value0 . '</th>';
+			    echo '<th onclick="sortTable(0)">' . $value0 . '</th>';
 			    echo '<th>' . $value1 . '</th>';
 			    echo '<th>' . $value2 . '</th>';
 			} else {
@@ -107,25 +107,39 @@
 	    <p>Read the <a href='https://gumroad.com/l/linux-photography'>Linux Photography</a> book</p>
 	</div>
 	<script>
-	 function sortTable() {
-	     var table, rows, switching, i, x, y, shouldSwitch;
-	     table = document.getElementById('theTable');
+	 function sortTable(n) {
+	     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	     table = document.getElementById("theTable");
 	     switching = true;
+	     dir = "asc"; 
 	     while (switching) {
 		 switching = false;
 		 rows = table.rows;
 		 for (i = 1; i < (rows.length - 1); i++) {
 		     shouldSwitch = false;
-		     x = rows[i].getElementsByTagName('TD')[0];
-		     y = rows[i + 1].getElementsByTagName('TD')[0];
+		     x = rows[i].getElementsByTagName("TD")[n];
+		     y = rows[i + 1].getElementsByTagName("TD")[n];
+		     if (dir == "asc") {
 			 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+			     shouldSwitch= true;
+			     break;
+			 }
+		     } else if (dir == "desc") {
+			 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
 			     shouldSwitch = true;
 			     break;
 			 }
+		     }
 		 }
 		 if (shouldSwitch) {
 		     rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
 		     switching = true;
+		     switchcount ++;      
+		 } else {
+		     if (switchcount == 0 && dir == "asc") {
+			 dir = "desc";
+			 switching = true;
+		     }
 		 }
 	     }
 	 }
