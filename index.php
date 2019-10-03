0d95c65ebe9fe5fb5fe330dd7c741e1306fbf0f4
Dmitri Popov <dmpop@linux.com>
2019-10-03 17:07:00 +0200

    Add individual product photos

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once('protect.php');
+?>
+
 <html lang="en">
     <!-- Author: Dmitri Popov, dmpop@linux.com
          License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
@@ -18,7 +22,7 @@
 	     color: #cc6600;
 	 }
 	 img {
-	     width:100%;
+	     width:100px;
 	     max-width:800px;
 	     border-radius: 9px;
 	 }
@@ -50,10 +54,9 @@
 		$CSVFILE = "data.csv";
 		if(!is_file($CSVFILE))
 		{
-		    $HEADER = "Item;Serial no.;Notes\nCameral Model;XXXXXX-XXXX;Note goes here";
+		    $HEADER = "Photo;Item;Serial no.;Notes\nandi.jpeg;Cameral Model;XXXXXX-XXXX;Note goes here";
 		    file_put_contents($CSVFILE, $HEADER);
 		}
-		$PHOTO="epc.jpg";
 		$row = 1;
 		if (($handle = fopen($CSVFILE, "r")) !== FALSE) {
 		    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
@@ -69,17 +72,23 @@
 			    $value0 = $data[0];
 			    $value1 = $data[1];
 			    $value2 = $data[2];
+			    $value3 = $data[3];
 			}
 			if ($row == 1) {
-			    echo '<th class="sortable" onclick="sortTable(0)">' . $value0 . '</th>';
+			    echo '<th>' . $value0 . '</th>';
 			    echo '<th>' . $value1 . '</th>';
 			    echo '<th>' . $value2 . '</th>';
+			    echo '<th>' . $value3 . '</th>';
 			} else {
-			    echo '<td>'.$value0.'</td>';
-			    echo '<td class="col1">'.$value1.'</td>';
-    			    echo '<td class="col2">'.$value2.'</td>';
+			    if ($value0=='na'){
+			        echo '<td><a href="img/andi.jpeg"><img src="img/andi.jpeg"/></a></td>';
+			    } else {
+			    echo '<td><a href="img/'.$value0.'"><img src="img/'.$value0.'"/></a></td>';
+			    }
+			    echo '<td>'.$value1.'</td>';
+    			echo '<td class="col1">'.$value2.'</td>';
+    			echo '<td class="col2">'.$value3.'</td>';
 			}
-			// }
 			if ($row == 1) {
 			    echo '</tr></thead><tbody>';
 			} else {
@@ -95,50 +104,7 @@
             <form method='GET' action='edit.php'>
 		<p><button type='submit'>Edit</button></p>
             </form>
-	    <?php
-	    if (file_exists($PHOTO)) {
-		echo "<br/><img src='epc.jpg'>";
-	    }
-	    ?>
 	    <p>Read the <a href='https://gumroad.com/l/linux-photography'>Linux Photography</a> book</p>
 	</div>
-	<script>
-	 function sortTable(n) {
-	     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-	     table = document.getElementById("theTable");
-	     switching = true;
-	     dir = "asc"; 
-	     while (switching) {
-		 switching = false;
-		 rows = table.rows;
-		 for (i = 1; i < (rows.length - 1); i++) {
-		     shouldSwitch = false;
-		     x = rows[i].getElementsByTagName("TD")[n];
-		     y = rows[i + 1].getElementsByTagName("TD")[n];
-		     if (dir == "asc") {
-			 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-			     shouldSwitch= true;
-			     break;
-			 }
-		     } else if (dir == "desc") {
-			 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-			     shouldSwitch = true;
-			     break;
-			 }
-		     }
-		 }
-		 if (shouldSwitch) {
-		     rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-		     switching = true;
-		     switchcount ++;      
-		 } else {
-		     if (switchcount == 0 && dir == "asc") {
-			 dir = "desc";
-			 switching = true;
-		     }
-		 }
-	     }
-	 }
-	</script>
     </body>
 </html>
