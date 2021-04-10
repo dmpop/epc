3476acdb72653c9f589f5cfecc49c5d96c2f9adb
Dmitri Popov <dmpop@linux.com>
2021-04-10 18:00:10 +0200

    Fix view error

--- start.sh
+++ start.sh
@@ -5,4 +5,8 @@ if [ ! -x "$(command -v php)" ]; then
     exit 1
 fi
 nohup php -S 0.0.0.0:8000 >/dev/null 2>&1 &
-xdg-open http://localhost:8000
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    xdg-open http://localhost:8000
+elif  [ "$(uname)" == "Darwin" ]; then
+    open http://localhost:8000
+fi
--- view.php
+++ view.php
@@ -3,7 +3,6 @@ include('config.php');
 if ($protect) {
     require_once('protect.php');
 }
-error_reporting(E_ALL ^ E_WARNING);
 ?>
 
 <html lang="en" data-theme="<?php echo $theme ?>">
@@ -41,24 +40,23 @@ error_reporting(E_ALL ^ E_WARNING);
             <?php
             $CSVFILE = "data.csv";
             $handle = fopen($CSVFILE, "r");
-            $i = 0;
             $row = (int) $_GET["item"];
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                if (++$i < $row) continue;
-                fclose($handle);
-                $value0 = $data[0];
-                $value1 = $data[1];
-                $value2 = $data[2];
-                $value3 = $data[3];
-                $value4 = $data[4];
-                echo '<h2 class=text-center" style="margin-top: 1em; margin-bottom: 1em;">' . $value1 . '</h2>';
-                echo '<div style="text-align: left;">';
-                echo '<img style="width: 100%;  border-radius: 7px; margin-top: 0.5em;" src="img/' . $value0 . '" />';
-                echo '<hr style="margin-top: 2em;">';
-                echo '<p><div style="color: gray;">Serial number:</div>' . $value2 . '</p>';
-                echo '<p><div style="color: gray;">Price (' . $currency . '):</div>' . number_format(floatval($value3), 2) . '</p>';
-                echo '<p><div style="color: gray;">Note:</div>' . $value4 . '</p>';
-                echo '</div>';
+            for($i = 1; $data = fgetcsv($handle, 1000, ";"); $i++) {
+                if($i === $row) {
+                    $value0 = $data[0];
+                    $value1 = $data[1];
+                    $value2 = $data[2];
+                    $value3 = $data[3];
+                    $value4 = $data[4];
+                    echo '<h2 class=text-center" style="margin-top: 1em; margin-bottom: 1em;">' . $value1 . '</h2>';
+                    echo '<div style="text-align: left;">';
+                    echo '<img style="width: 100%;  border-radius: 7px; margin-top: 0.5em;" src="img/' . $value0 . '" />';
+                    echo '<hr style="margin-top: 2em;">';
+                    echo '<p><div style="color: gray;">Serial number:</div>' . $value2 . '</p>';
+                    echo '<p><div style="color: gray;">Price (' . $currency . '):</div>' . number_format(floatval($value3), 2) . '</p>';
+                    echo '<p><div style="color: gray;">Note:</div>' . $value4 . '</p>';
+                    echo '</div>';
+                }
             }
             ?>
         </div>
